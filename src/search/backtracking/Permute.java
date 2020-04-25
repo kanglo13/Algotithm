76166f9539e1b18be9129e00ad90dd79202762f7
kanglo13 <luogangklj@gmail.com>
2020-04-25 10:28:21 +0800

    coin Change

--- src/search/backtracking/Permute.java
+++ src/search/backtracking/Permute.java
@@ -1,4 +1,29 @@
 package search.backtracking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Permute {
+    public List<List<Integer>> permute(int[]nums){
+        List<List<Integer>> permute = new ArrayList<>();
+        List<Integer> permuteList = new ArrayList<>();
+        boolean[]hasVisited = new boolean[nums.length];
+        backtracking(permute,permuteList,nums,hasVisited);
+        return permute;
+    }
+    private void backtracking(List<List<Integer>>permute,List<Integer> permuteList,int[]nums,boolean[]hasVisited){
+        if (permuteList.size() == nums.length){
+            permute.add(new ArrayList<>(permuteList));
+            return;
+        }
+        for (int i = 0;i < nums.length;i++){
+            if (hasVisited[i])
+                continue;
+            permuteList.add(nums[i]);
+            hasVisited[i] = true;
+            backtracking(permute,permuteList,nums,hasVisited);
+            permuteList.remove(permuteList.size()-1);
+            hasVisited[i] = false;
+        }
+    }
 }
