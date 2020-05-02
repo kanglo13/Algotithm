053761444c1a29c5e728eac13e17a6a0e4a6ec5f
kanglo <luogangklj@gmail.com>
2020-05-02 23:15:01 +0800

    substring

---
+++ src/daily/MergeTwoLists.java
@@ -0,0 +1,17 @@
+package daily;
+public class MergeTwoLists {
+    public ListNode mergeTwoList(ListNode l1,ListNode l2){
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+        if (l1.val <= l2.val){
+            l1.next = mergeTwoList(l1.next,l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoList(l1,l2.next);
+            return l2;
+        }
+    }
+}
