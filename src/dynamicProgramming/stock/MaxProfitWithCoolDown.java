c4c927aea1a0d8f8efe3840c1c774d3e2af78ddb
kanglo <luogangklj@gmail.com>
2020-05-05 15:27:06 +0800

    repost

--- src/dynamicProgramming/stock/MaxProfitWithCoolDown.java
+++ src/dynamicProgramming/stock/MaxProfitWithCoolDown.java
@@ -1,5 +1,7 @@
 package dynamicProgramming.stock;
 
+import jdk.jshell.spi.SPIResolutionException;
+
 public class MaxProfitWithCoolDown {
     public int maxProfit(int[] prices){
         if (prices == null || prices.length == 0)
@@ -7,9 +9,14 @@ public class MaxProfitWithCoolDown {
         int n = prices.length;
         int[][] dp = new int[n][2];
         for (int i = 0;i < n;i++){
-            if (i - 1 == -1 || i-2 == -1){
+            if (i - 1 == -1){
                 dp[i][0] = 0;
                 dp[i][1] = -prices[i];
+                continue;
+            }
+            if (i - 2 == -1){
+                dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1] + prices[i]);
+                dp[i][1] = Math.max(dp[i-1][1],dp[i-1][0] - prices[i]);
                 continue;
             }
             dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1] + prices[i]);
