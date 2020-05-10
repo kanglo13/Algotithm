350e9574cb0d5efaba296c3621c513d09d64bcd8
kanglo <luogangklj@gmail.com>
2020-05-10 18:22:33 +0800

    daily checkin

--- src/daily/LowestCommonAncestor.java
+++ src/daily/LowestCommonAncestor.java
@@ -2,7 +2,7 @@ package daily;
 
 public class LowestCommonAncestor {
     public TreeNode lowestCommonAncestor(TreeNode root,TreeNode p,TreeNode q){
-        if (root == null || p == null || q == null)
+        if (root == null ||root == p || root == q)
             return root;
         TreeNode left = lowestCommonAncestor(root.left,p,q);
         TreeNode right = lowestCommonAncestor(root.right,p,q);
