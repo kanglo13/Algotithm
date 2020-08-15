08aae9054369de2e56dd2b8ca4b6aff5b49fa7a5
kanlo13 <luogangklj@gmail.com>
2020-08-15 11:14:04 +0800

    checkin 8.15

---
+++ src/dynamicProgramming/zone/RemoveBoxes.java
@@ -0,0 +1,27 @@
+package dynamicProgramming.zone;
+
+public class RemoveBoxes {
+   private int dp[][][];
+   private int[]boxes;
+   public int removeBoxes(int[]boxes){
+       int n = boxes.length;
+       dp = new int[n][n][n];
+       this.boxes = boxes;
+   }
+   private int dfs(int i,int j,int k){
+       if (i > j)
+           return 0;
+       if (dp[i][j][k] > 0)
+           return dp[i][j][k];
+       while (i < j && boxes[i] == boxes[i+1]){
+           i++;
+           k++;
+       }
+       int res = (k+1) * (k + 1) + dfs(i+1,j,0);
+       for (int m = i+1;m <= j;m++){
+           if (boxes[m] == boxes[i])
+               res = Math.max(res,dfs(i+1,m-1,0)+dfs(m,j,k+1));
+       }
+       return dp[i][j][k] = res;
+   }
+}
