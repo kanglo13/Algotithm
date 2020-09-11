c5ba8ddd6ee1bd591e6bafd4ee59a034329ddcfb
kanlo13 <luogangklj@gmail.com>
2020-09-11 09:36:44 +0800

    checkin 9.11

--- src/daily/CombinationSum3.java
+++ src/daily/CombinationSum3.java
@@ -1,7 +1,31 @@
 package daily;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
-    *@author kanglo
-    *@create 2020-09-2020/9/11 9:17
-*/
-    public class CombinationSum3 {
+ * @author kanglo
+ * @create 2020-09-2020/9/11 9:17
+ */
+public class CombinationSum3 {
+    public List<List<Integer>>combinationSum3(int k,int n){
+        List<List<Integer>>combinations = new ArrayList<>();
+        if (k <=0 || n <= 0)
+            return combinations;
+        backtracking(combinations,new ArrayList<>(),k,n,1);
+        return combinations;
+    }
+    private void backtracking(List<List<Integer>>combinations,List<Integer>list,int k,int n,int start){
+        if (list.size() == k && n == 0 ){
+            combinations.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = start;i <= 9;i++){
+            if (i <= n){
+                list.add(i);
+                backtracking(combinations,list,k,n-i,i+1);
+                list.remove(list.size()-1);
+            }
+        }
+    }
 }
