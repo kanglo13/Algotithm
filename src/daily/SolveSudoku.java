c782330fd2d77eeb52f5e2d333a0d68bcf22b123
kanlo13 <luogangklj@gmail.com>
2020-09-15 10:40:21 +0800

    checkin 9.15

---
+++ src/daily/SolveSudoku.java
@@ -0,0 +1,51 @@
+package daily;
+
+/**
+ * @author kanglo
+ * @create 2020-09-2020/9/15 9:58
+ */
+public class SolveSudoku {
+    private boolean[][] rowUsed = new boolean[9][10];
+    private boolean[][] colUsed = new boolean[9][10];
+    private boolean[][] cubeUsed = new boolean[9][10];
+    private char[][]board;
+    public void solveSudoku(char[][]board){
+        this.board = board;
+        for (int i = 0;i < 9;i++){
+            for (int j = 0;j < 9;j++){
+                if (board[i][j] == '.')
+                    continue;
+                int num = board[i][j] - '0';
+                rowUsed[i][num] = true;
+                colUsed[j][num] = true;
+                cubeUsed[cubeNum(i,j)][num] = true;
+            }
+        }
+        backtracking(0,0);
+    }
+    private boolean backtracking(int row,int col){
+        while (row < 9 && board[row][col] != '.'){
+            row = col == 8 ? row + 1 : row;
+            col = col == 8 ? 0 : col + 1;
+        }
+        if (row == 9)
+            return true;
+        for (int num = 1;num <= 9;num++){
+            if (rowUsed[row][num] || colUsed[col][num] || cubeUsed[cubeNum(row,col)][num])
+                continue;
+            rowUsed[row][num] = colUsed[col][num] = cubeUsed[cubeNum(row,col)][num] = true;
+            board[row][col] = (char)(num + '0');
+            if (backtracking(row,col))
+                return true;
+            board[row][col] = '.';
+            rowUsed[row][num] = colUsed[col][num] = cubeUsed[cubeNum(row,col)][num] = false;
+        }
+        return false;
+    }
+
+    private int cubeNum(int i,int j){
+        int r = i / 3;
+        int c = j / 3;
+        return r * 3 + c;
+    }
+}
